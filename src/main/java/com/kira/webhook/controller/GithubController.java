b779f57a10948acd529f14ea96eef066c994de6a
Natdanai Nitipan <c3bosskung@gmail.com>
2024-04-20 11:03:43 +0700

    init project

--- src/main/java/com/kira/webhook/controller/GithubController.java
+++ src/main/java/com/kira/webhook/controller/GithubController.java
@@ -1,16 +1,14 @@
 package com.kira.webhook.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/github")
 public class GithubController {
 
-    @GetMapping("/assignee")
-    public String assignee() {
-        return "Assignee";
+    @PostMapping("/assignee")
+    public String assignee(@RequestBody String body) {
+        return "body: " + body;
     }
 
 }
